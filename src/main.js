afe74bf18640d7480e42b0702924b90794aa0a6c
wsh <3443327820@qq.com>
2023-02-15 22:30:01 +0800

    style:自定义ol控件缩放样式

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import "nprogress/nprogress.css";
 import App from "./App.vue";
 import "ol/ol.css";
+import "@/assets/css/custom.less";
 import "lib-flexible/flexible.js";
 import router from "./router";
 createApp(App).use(router).mount("#app");
